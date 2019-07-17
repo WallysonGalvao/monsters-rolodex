d778cc15416e2ee4edd710c1963c6e3c4e74b285
Wallyson Galvão <wallyson.galvao@gmail.com>
2019-07-16 21:49:28 -0300

    Updates

---
+++ precache-manifest.9c4b92191d30388f70ba08739b9d798a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62997b8d25fae9971ee54d4c9301589",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9dea9ec4e3f652ff1f2b",
+    "url": "/monsters-rolodex/static/css/main.ed523ca9.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/monsters-rolodex/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "9dea9ec4e3f652ff1f2b",
+    "url": "/monsters-rolodex/static/js/main.303c363d.chunk.js"
+  },
+  {
+    "revision": "49006584ddd3bfbd8db8",
+    "url": "/monsters-rolodex/static/js/runtime~main.26edc6c9.js"
+  }
+]);
